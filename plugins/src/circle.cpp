b1e9d9e4b9155838efefd52b72f183f436e4e8e1
Ajay Shrestha <ivaresaki@gmail.com>
2025-03-06 13:59:43 -0500

    added circle plugin. Needs few mod to function symbol loading

---
+++ plugins/src/circle.cpp
@@ -0,0 +1,33 @@
+#include "circle.hpp"
+
+extern "C" Shape* create_shape(double radius)
+{
+  return new Circle(radius);
+}
+
+Circle::Circle(double radius)
+  : Shape("Circle"), radius(radius){}
+
+Circle::~Circle(){}
+
+double Circle::get_radius() const
+{
+  return this->radius;
+}
+
+double Circle::get_area() const
+{
+  return this->PI * this->get_radius() * this->get_radius();
+}
+
+double Circle::get_perimeter() const
+{
+  return 2 * this->PI * this->get_radius();
+}
+
+std::ostream& Circle::print(std::ostream& os) const
+{
+  Shape::print(os);
+  os << "Radius: " << this->get_radius() << std::endl;
+  return os;
+}
